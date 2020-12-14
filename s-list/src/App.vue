327c470e703c902ee4a3f51cf096b312de8822bc
you86 <r115322@g.kobe-kosen.ac.jp>
2020-12-14 14:20:43 +0900

    studyでやっていた内容の反映。テキストボックスが二回目以降クリアできないバグを解消。

---
+++ s-list/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-container>
+       <v-row class="grey lighten-3">
+        <v-col cols="3" style="background-color: #FFCDD2">
+         <Test/>
+        </v-col>
+        <v-col cols="9" style="background-color: #F8BBD0">
+          <HelloWorld/>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+import Test from './components/Test';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Test,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ s-list/src/components/Test.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <v-text-field
+        label="New Task"
+        hide-details="auto"
+        v-model="task"
+        ></v-text-field>
+        <v-btn
+        elevation="2"
+        @click="addTask"
+        class="ma-3"
+        >Add</v-btn>
+        <v-card
+            class="mx-auto"
+            max-width="300"
+            tile
+        >
+            <v-list dense>
+            <v-subheader>My Tasks</v-subheader>
+            <v-list-item-group
+                color="primary"
+            >
+                <v-list-item
+                v-for="(item, i) in items"
+                :key="i"
+                >
+                    <v-list-item-content>
+                        <v-list-item-title v-text="item.text"></v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list-item-group>
+            </v-list>
+        </v-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            items: [],
+            task: ''
+        }),
+        methods:{
+            addTask(){
+                if(this.task.length != 0)this.items.push({"text":this.task});
+                this.task = '';
+            }
+        }
+    }
+</script>
