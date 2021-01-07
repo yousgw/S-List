43be0487f229f9bc8be835d634dc583111c7140c
you86 <r115322@g.kobe-kosen.ac.jp>
2021-01-07 23:58:31 +0900

    実際に使う用の期日あり、なしそれぞれ取り出すように関数を作成、いまだ途中。

--- backend/test.go
+++ backend/test.go
@@ -15,39 +15,33 @@ type TODO struct {
 	Islimited bool
 }
 
-func input() {
-	db, err := sql.Open("postgres", "user=postgres password=seriver25859 dbname=todo sslmode=disable")
-	defer db.Close()
-	if err != nil {
-		fmt.Println(err)
-	}
+// 更新時の参照用としてTaskidとか作ったほうがいい
 
-	stmt, err := db.Prepare("INSERT INTO todos VALUES ($1,$2,$3,$4,$5);")
+func checkErr(err error) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	res, err := stmt.Exec("2020-1-3", "test2", "testTask2", 1, 1)
-	if err != nil {
-		fmt.Println(err)
-	}
-	id, err := res.LastInsertId()
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(id)
 }
 
-func output() {
+func in(t TODO) {
 	db, err := sql.Open("postgres", "user=postgres password=seriver25859 dbname=todo sslmode=disable")
 	defer db.Close()
-	if err != nil {
-		fmt.Println(err)
-	}
+	checkErr(err)
 
-	rows, err := db.Query("SELECT * FROM todos")
-	if err != nil {
-		fmt.Println(err)
-	}
+	stmt, err := db.Prepare("INSERT INTO todos VALUES ($1,$2,$3,$4,$5);")
+	checkErr(err)
+	res, err := stmt.Exec(t.Limit, t.Title, t.Intro, t.Share, t.Islimited)
+	checkErr(err)
+}
+
+// 期日順で出力
+func outAll() {
+	db, err := sql.Open("postgres", "user=postgres password=seriver25859 dbname=todo sslmode=disable")
+	defer db.Close()
+	checkErr(err)
+
+	rows, err := db.Query("SELECT * FROM todos order by ")
+	checkErr(err)
 	for rows.Next() {
 		t := TODO{}
 		rows.Scan(&t.Limit, &t.Title, &t.Intro, &t.Share, &t.Islimited)
@@ -55,8 +49,28 @@ func output() {
 	}
 }
 
+// 期日ありのタスクを期日順で出力
+func outLimited() {
+	db, err := sql.Open("postgres", "user=postgres password=seriver25859 dbname=todo sslmode=disable")
+	defer db.Close()
+	checkErr(err)
+
+	rows, err := db.Query("SELECT * FROM todos order by XXX WHERE YYY")
+	checkErr(err)
+	for rows.Next() {
+		t := TODO{}
+		rows.Scan(&t.Limit, &t.Title, &t.Intro, &t.Share, &t.Islimited)
+		fmt.Println(t)
+	}
+}
+
+// 期日なしのタスクを期日(作成日)順で出力
+func outUnlimited() {
+
+}
+
 func main() {
-	// input()
-	output()
+	// in()
+	outAll()
 
 }
